4e727527254b89f7bf5cdd2d87b895d55795a1a7
Grant Paul <chpwn@chpwn.com>
2011-01-16 21:13:03 -0800

    More failed attempts to get it working :(.

--- VAPlugin.mm
+++ VAPlugin.mm
@@ -1,12 +1,23 @@
 
 #import <objc/runtime.h>
+#import "VAShared.h"
+
+
 @protocol VSRecognitionResultHandler;
+
+__attribute__((constructor)) static void VAPluginInit() {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    NSLog(@"testing loaded vsbundle");
+
+    [pool release];
+}
 
 @interface VAResultHandler : NSObject <VSRecognitionResultHandler>  { } @end
 @implementation VAResultHandler
 - (id)actionForRecognitionResult:(id)recognitionResult {
     id act = [[objc_getClass("VSRecognitionSpeakAction") alloc] initWithSpokenFeedbackString:@"its time to go to bed" willTerminate:NO];
-    NSLog(@"hello to va");
+    NSLog(@"recognized result");
     return [act autorelease];
 }
 @end
